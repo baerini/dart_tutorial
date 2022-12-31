5210d7d2635461a3667c7852cb572428e042d2a4
baerini <rudduqlek@naver.com>
2022-12-31 22:25:02 +0900

    for if while

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,22 @@
 void main() {
-  bool visible = true;
-  print(visible);
+  if (true) {
+    print("참");
+  } else {
+    print('거짓');
+  }
 
-  int number = 17 ~/ 4; // 몫 구하기
-  print(number);
-
-  double pi = 3.14;
-  print(pi);
-
-  String name = '홍길동';
-  print(name);
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
 
   List ages = [10, 11, 12];
-  print(ages);
+  for (var age in ages) {
+    print(age);
+  }
+  //recent repeat method
 
-  Map person = {'name': '홍길동', 'age': 12};
-  print(person);
-
-  final pi2 = 3.14;
-  print(pi2);
-
-  const pi3 = 3.14;
-  print(pi3);
-
-  //final == const final은 클래스 호출시 생성자에서 배정받은 그 값을
-  //고정으로 가져가고 const는 즉시 초기화 즉시 고정
-  //var는 대입되는 값에 타입을 맞춰줌
+  int year = 2010;
+  while (year < 2016) {
+    year += 1;
+  }
 }
